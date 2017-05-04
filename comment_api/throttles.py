3e2a0bfcb5102e1fe75d79c8d348ef09aefee576
SeanAppleby <seanappleby@comcast.net>
2017-05-04 17:01:38 -0400

    cleaned up combined throttle

--- comment_api/throttles.py
+++ comment_api/throttles.py
@@ -126,36 +126,15 @@ class PostThrottle(throttling.BaseThrottle):
         return False
 
 
-class CombinedThrottle(throttling.BaseThrottle):
+class CombinedThrottle(throttling.UserRateThrottle):
     """
-    A simple cache implementation, that only requires `.get_cache_key()`
-    to be overridden.
-    The rate (requests / seconds) is set by a `throttle` attribute on the View
-    class.  The attribute is a string of the form 'number_of_requests/period'.
-    Period should be one of: ('s', 'sec', 'm', 'min', 'h', 'hour', 'd', 'day')
-    Previous request information used for throttling is stored in the cache.
+    This is the standard UserRateThrottle, just with rate pulled from the db.
+    This is done so that an admin can adjust the value at runtime.
     """
     cache = default_cache
     timer = time.time
     cache_format = 'combined_throttle_%(scope)s_%(ident)s'
     scope = None
-
-
-    def __init__(self):
-        if not getattr(self, 'rate', None):
-            self.rate = self.get_rate()
-        self.num_requests, self.duration = self.parse_rate(self.rate)
-
-    def get_cache_key(self, request, view):
-        if is_authenticated(request.user):
-            ident = request.user.pk
-        else:
-            ident = self.get_ident(request)
-
-        return self.cache_format % {
-            'scope': self.scope,
-            'ident': ident
-        }
 
     def get_rate(self):
         """
@@ -167,69 +146,3 @@ class CombinedThrottle(throttling.BaseThrottle):
         except KeyError:
             msg = "No default throttle rate set for '%s' scope" % self.scope
             raise ImproperlyConfigured(msg)
-
-    def parse_rate(self, rate):
-        """
-        Given the request rate string, return a two tuple of:
-        <allowed number of requests>, <period of time in seconds>
-        """
-        if rate is None:
-            return (None, None)
-        num, period = rate.split('/')
-        num_requests = int(num)
-        duration = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}[period[0]]
-        return (num_requests, duration)
-
-    def allow_request(self, request, view):
-        """
-        Implement the check to see if the request should be throttled.
-        On success calls `throttle_success`.
-        On failure calls `throttle_failure`.
-        """
-        if self.rate is None:
-            return True
-
-        self.key = self.get_cache_key(request, view)
-        if self.key is None:
-            return True
-
-        self.history = self.cache.get(self.key, [])
-        self.now = self.timer()
-
-        # Drop any requests from the history which have now passed the
-        # throttle duration
-        while self.history and self.history[-1] <= self.now - self.duration:
-            self.history.pop()
-        if len(self.history) >= self.num_requests:
-            return self.throttle_failure()
-        return self.throttle_success()
-
-    def throttle_success(self):
-        """
-        Inserts the current request's timestamp along with the key
-        into the cache.
-        """
-        self.history.insert(0, self.now)
-        self.cache.set(self.key, self.history, self.duration)
-        return True
-
-    def throttle_failure(self):
-        """
-        Called when a request to the API has failed due to throttling.
-        """
-        return False
-
-    def wait(self):
-        """
-        Returns the recommended next request time in seconds.
-        """
-        if self.history:
-            remaining_duration = self.duration - (self.now - self.history[-1])
-        else:
-            remaining_duration = self.duration
-
-        available_requests = self.num_requests - len(self.history) + 1
-        if available_requests <= 0:
-            return None
-
-        return remaining_duration / float(available_requests)
